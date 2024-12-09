90215ad002bcf6c9766ea8f5035b50fb7b5b50e7
aZina0 <74600597+aZina0@users.noreply.github.com>
2024-12-09 21:19:16 +0100

    Grid layout

--- database_edit.py
+++ database_edit.py
@@ -1,6 +1,6 @@
 from PySide6.QtWidgets import *
+from PySide6.QtCore import Signal
 import sys
-import sqlite3
 import os.path
 import initialize_database
 
@@ -15,58 +15,88 @@ if not os.path.exists("database.db"):
 sys.argv += ['-platform', 'windows:darkmode=2']
 app = QApplication(sys.argv)
 app.setStyle('Fusion')
+app.setStyleSheet(open('styles.css').read())
 
-window = QMainWindow()
 
+class Window(QMainWindow):
+	resize_signal = Signal(int, int)
+
+	def __init__(self):
+		super().__init__()
+		self.setFixedSize(1000, 750)
+		self.setMinimumSize(400, 400)
+
+	def resizeEvent(self, event):
+		width = event.size().width()
+		height = event.size().height()
+		self.resize_signal.emit(width, height)
+		super().resizeEvent(event)
 
 
 class CategoryEditWidget(QWidget):
 	def __init__(self, parent):
 		super().__init__(parent)
-		self.setGeometry(0, 0, 700, 700)
-		self.setStyleSheet('background-color: darkred')
-
-		self.add_widget = QWidget(self)
-		self.add_text = QLineEdit("ime kategorije: ", self.add_widget)
-		self.add_button = QPushButton("Dodaj kategoriju", self.add_widget)
-
-		self.rename_remove_layout = QGridLayout(self)
-		self.rename_remove_widget = QWidget(self)
-		# self.rename_remove_widget.col
-		self.rename_remove_widget.setLayout(self.rename_remove_layout)
-
-		self.list = QScrollArea(self.rename_remove_widget)
-		self.rename_remove_layout.addWidget(self.list, 0, 0, 3, 2)
-
-		self.rename_text = QLineEdit("Novo ime kategorije: ", self.rename_remove_widget)
-		self.rename_remove_layout.addWidget(self.rename_text, 0, 2, 1, 1)
-
-		self.rename_button = QPushButton("Preimenuj kategoriju: ", self.rename_remove_widget)
-		self.rename_remove_layout.addWidget(self.rename_button, 1, 2, 1, 1)
-
-		self.remove_button = QPushButton("Ukloni kategoriju")
-		self.rename_remove_layout.addWidget(self.remove_button, 2, 2, 1, 1)
-
-
-		self.rename_remove_widget.setFixedSize(self.rename_remove_layout.sizeHint())
-
-
-		
+		self.setMinimumSize(700, 400)
 		self.layout = QGridLayout(self)
-		self.setLayout(self.layout)
-		self.layout.addWidget(self.add_widget, 0, 0)
-		self.layout.addWidget(self.rename_remove_widget, 0, 1)
+		self.layout.setRowStretch(0, 2)
+		self.layout.setRowStretch(1, 2)
+		self.layout.setRowStretch(2, 1)
+		self.layout.setColumnStretch(0, 1)
+		self.layout.setColumnStretch(1, 1)
 
+
+		self.list_widget = QGroupBox("Lista kategorija", self)
+		self.list_layout = QVBoxLayout(self.list_widget)
+		self.layout.addWidget(self.list_widget, 0, 0, 3, 1)
+
+		self.list = QScrollArea(self)
+		self.list_layout.addWidget(self.list)
+
+
+		self.add_widget = QGroupBox("Dodaj kategoriju", self)
+		self.add_layout = QVBoxLayout(self.add_widget)
+		self.layout.addWidget(self.add_widget, 0, 1)
+
+		self.add_text = QLineEdit("ime kategorije: ")
+		self.add_layout.addWidget(self.add_text)
+
+		self.add_button = QPushButton("Dodaj")
+		self.add_button.setStyleSheet('background-color: darkgreen; margin-left: 150')
+		self.add_layout.addWidget(self.add_button)
+
+
+		self.rename_widget = QGroupBox("Preimenuj kategoriju", self)
+		self.rename_layout = QVBoxLayout(self.rename_widget)
+		self.layout.addWidget(self.rename_widget, 1, 1)
+
+		self.rename_text = QLineEdit("Novo ime kategorije: ")
+		self.rename_layout.addWidget(self.rename_text)
+
+		self.rename_button = QPushButton("Preimenuj")
+		self.rename_button.setStyleSheet('margin-left: 150')
+		self.rename_layout.addWidget(self.rename_button)
+
+
+		self.remove_widget = QGroupBox("Izbriši kategoriju", self)
+		self.remove_layout = QVBoxLayout(self.remove_widget)
+		self.layout.addWidget(self.remove_widget, 2, 1)
+
+		self.remove_button = QPushButton("Izbriši")
+		self.remove_button.setStyleSheet('background-color: darkred; margin-left: 150')
+		self.remove_layout.addWidget(self.remove_button)
+
+
+	def window_resized(self, width, height):
+		self.setFixedSize(width, height)
+
+
+
+
+window = Window()
 
 category_edit_widget = CategoryEditWidget(window)
+window.resize_signal.connect(category_edit_widget.window_resized)
 
-
-
-window.setGeometry(0, 0, 1000, 750)
 window.show()
-
-
-
-
 
 app.exec()
